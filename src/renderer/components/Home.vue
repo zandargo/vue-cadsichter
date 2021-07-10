77e500a7a7f9ec0a8650a657f2cea170cd8b5c4a
Madson Germano <madson.br@gmail.com>
2021-07-10 12:31:58 -0300

    wip

---
+++ src/renderer/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<h1>Teste...</h1>
+</template>
+
+<script>
+export default {
+	name: 'Home',
+}
+</script>
+
+<style lang="scss">
+@import '@/index';
+
+h1 {
+	color: $main-color;
+}
+</style>
