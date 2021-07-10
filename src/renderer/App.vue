aa8ae1f9225d082271e76e1987efe86438caf6a8
Madson Germano <madson.br@gmail.com>
2021-07-10 11:22:34 -0300

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="app">
+		<!-- <landing-page></landing-page> -->
+		<Home />
+	</div>
+</template>
+
+<script>
+//_ import LandingPage from '@/components/LandingPage'
+import Home from '@/components/Home'
+
+export default {
+	name: 'vue-cadsichter',
+	components: {
+		//_ LandingPage,
+		Home,
+	},
+}
+</script>
+
+<style lang="scss">
+@charset "utf-8";
+@import '@/styles/variables';
+.vue-logo {
+	max-width: 10rem;
+}
+</style>
---
+++ src/renderer/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+
+import App from './App'
+import store from './store'
+
+import '@/index.scss'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+	components: { App },
+	store,
+	template: '<App/>',
+}).$mount('#app')
